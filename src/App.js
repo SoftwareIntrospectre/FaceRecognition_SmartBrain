320caa88984c32ba6d44a2f688d624745aa953f3
Software Introspectre <softwareintrospectre@gmail.com>
2018-12-11 07:54:01 -0500

    Adding facial recognition face, and start of Sign In form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import Navigation from './Components/Navigation/Navigation';
+import SignIn from './Components/SignIn/SignIn';
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
@@ -29,8 +30,30 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageURL: ''
+      imageURL: '',
+      box: {},
+      //keeps track of where user is on the webpage
+      route: 'signin',
     }
+  }
+
+  calculateFaceLocation = (data) => {
+   const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+   const image = document.getElementById('inputimage');
+   const width = Number(image.width);
+   const height = Number(image.height);
+   return{
+    //percentages
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: width - (clarifaiFace.right_col * width),
+    bottomRow: height - (clarifaiFace.bottom_row * height)
+   }
+  }
+
+  displayFaceBox = (box) =>{
+    console.log(box);
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
@@ -43,15 +66,8 @@ class App extends Component {
     app.models.predict(
      Clarifai.FACE_DETECT_MODEL
       , this.state.input)
-      .then(
-        function(response) {
-          // do something with response
-          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-        },
-        function(err) {
-          // there was an error
-        }
-  );
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   }
 
   render() {
@@ -60,11 +76,18 @@ class App extends Component {
       <Particles className='particles'
         params={particlesOptions}/>
       <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm onInputChange = {this.onInputChange} 
-                     onButtonSubmit={this.onButtonSubmit} /> 
-      <FaceRecognition imageURL={this.state.imageURL}/>
+      
+      { //conditional statement. If state is 'SignIn', then use SignIn component, else do everything else
+        this.state.route === 'signin' ?
+          <SignIn />
+          : <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm  onInputChange = {this.onInputChange} 
+                            onButtonSubmit={this.onButtonSubmit} /> 
+              <FaceRecognition box ={this.state.box} imageURL={this.state.imageURL}/>
+            </div>
+      }
       </div>
     );
   }
