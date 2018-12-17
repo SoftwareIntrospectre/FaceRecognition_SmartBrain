4edf9e72f417e45119f1cb61fa80bba9907f5853
Software Introspectre <softwareintrospectre@gmail.com>
2018-12-17 07:32:57 -0500

    Finishing front end

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import Navigation from './Components/Navigation/Navigation';
 import SignIn from './Components/SignIn/SignIn';
+import Register from './Components/Register/Register';
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
@@ -34,6 +35,7 @@ class App extends Component {
       box: {},
       //keeps track of where user is on the webpage
       route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -43,16 +45,16 @@ class App extends Component {
    const width = Number(image.width);
    const height = Number(image.height);
    return{
-    //percentages
-    leftCol: clarifaiFace.left_col * width,
-    topRow: clarifaiFace.top_row * height,
-    rightCol: width - (clarifaiFace.right_col * width),
-    bottomRow: height - (clarifaiFace.bottom_row * height)
+      //percentages
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
    }
   }
 
   displayFaceBox = (box) =>{
-    console.log(box);
+    //console.log(box);
     this.setState({box: box});
   }
 
@@ -70,23 +72,45 @@ class App extends Component {
       .catch(err => console.log(err));
   }
 
+  onRouteChange = (route) => {
+    if(route === 'signout'){
+      this.setState({isSignedIn: false})
+    } else if (route === 'home'){
+      this.setState({isSignedIn: true})
+    }
+
+    this.setState({route: route});
+  }
+
   render() {
+    const {isSignedIn, imageURL, route, box} = this.state
     return (
       <div className="App">
       <Particles className='particles'
-        params={particlesOptions}/>
-      <Navigation />
+        params={particlesOptions}
+        />
+
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
       
-      { //conditional statement. If state is 'SignIn', then use SignIn component, else do everything else
-        this.state.route === 'signin' ?
-          <SignIn />
-          : <div>
+      { //conditional statement. If state is 'home', show the Home screen
+        this.state.route === 'home' 
+        ? <div>
               <Logo />
               <Rank />
               <ImageLinkForm  onInputChange = {this.onInputChange} 
                             onButtonSubmit={this.onButtonSubmit} /> 
-              <FaceRecognition box ={this.state.box} imageURL={this.state.imageURL}/>
+              <FaceRecognition box ={box} imageURL={this.state.imageURL}/>
             </div>
+
+          //else if state is 'signin', show the Sign In screen
+          : (
+              route === 'signin'
+            
+         ? <SignIn onRouteChange={this.onRouteChange} />
+
+         //else show the 'register' screen
+         : <Register onRouteChange = {this.onRouteChange}/>
+         )
       }
       </div>
     );
--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import './FaceRecognition.css';
 
-const FaceRecognition = ({imageURL, box}) => {
+const FaceRecognition = ({imageURL}) => {
 	return(
 			<div className = 'center ma'>
 				<div className ='absolute mt2'>
-					<img id ='inputimage' alt='' src={imageURL} width='500px' height='auto' />
-					<div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+					<img alt='' src={imageURL} width='500px' height='auto' />
 				</div>
 			</div>
 		);
